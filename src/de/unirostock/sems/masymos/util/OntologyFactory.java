4d78fcd34cf1c3ad21190cd837e889530ba17111
mp487 <martin.peters3@uni-rostock.de>
2015-12-09 16:34:31 +0100

    OntologyFactory now handles prior unknown ontology names

--- src/de/unirostock/sems/masymos/util/OntologyFactory.java
+++ src/de/unirostock/sems/masymos/util/OntologyFactory.java
@@ -1,143 +1,174 @@
 package de.unirostock.sems.masymos.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
 import org.neo4j.graphdb.index.UniqueFactory;
 
 import de.unirostock.sems.masymos.database.Manager;
 
 public class OntologyFactory {
-	
-	
+
+
 	static UniqueFactory<Node> SBOFactory;   
-    final static String indexSBO = "SBOOntology";
-    
+	final static String indexSBO = "SBOOntology";
+
 	static UniqueFactory<Node> KISAOFactory;   
-    final static String indexKISAO = "KISAOOntology";
-    
-    static UniqueFactory<Node> GOFactory;
-    final static String indexGO = "GOOntology";
-    
-    static UniqueFactory<Node> ChebiFactory;
-    final static String indexChebi = "ChebiOntology";
-    
-	static UniqueFactory<Node> UnnamedFactory;   
-    
-    public static UniqueFactory<Node> getFactory(String ontologyName) {
-    	
-    	switch (ontologyName) {
+	final static String indexKISAO = "KISAOOntology";
+
+	static UniqueFactory<Node> GOFactory;
+	final static String indexGO = "GOOntology";
+
+	static UniqueFactory<Node> ChebiFactory;
+	final static String indexChebi = "ChebiOntology";
+
+	static Map<String, UniqueFactory<Node>> factoryMap = new HashMap<>();
+
+	public static UniqueFactory<Node> getFactory(String ontologyName) {
+
+		switch (ontologyName) {
 		case indexSBO: return getSBOFactory();
-			
+
 		case indexGO: return getGOFactory();
-		
+
 		case indexKISAO: return getKISAOFactory();
-		
+
 		case indexChebi: return getChebiFactory();
-			
-		default: return getDefaultFactory();
+
+		default: return getOrCreateFactoryByName(ontologyName);
 		}
-    	
-    }
-	
- 
+
+	}
+
+
 	private static UniqueFactory<Node> getChebiFactory() {
-    	if (ChebiFactory == null){
-    		
-    		ChebiFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexChebi )
-    	    {
-    	        @Override
-    	        protected void initialize( Node created, Map<String, Object> properties )
-    	        {
-    	            created.setProperty( "id", properties.get( "id" ) );
-    	           created.addLabel(DynamicLabel.label(indexChebi));
-    	        }
-    	    };
-    	}
-    	
-    	return ChebiFactory;
+		if (ChebiFactory == null){
+
+			ChebiFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexChebi )
+			{
+				@Override
+				protected void initialize( Node created, Map<String, Object> properties )
+				{
+					created.setProperty( "id", properties.get( "id" ) );
+					created.addLabel(DynamicLabel.label(indexChebi));
+				}
+			};
+		}
+
+		return ChebiFactory;
 	}
 
 
 	private static UniqueFactory<Node> getSBOFactory() {
-		
-    	if (SBOFactory == null){
-    		
-    		SBOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexSBO )
-    	    {
-    	        @Override
-    	        protected void initialize( Node created, Map<String, Object> properties )
-    	        {
-    	            created.setProperty( "id", properties.get( "id" ) );
-    	           created.addLabel(DynamicLabel.label(indexSBO));
-    	        }
-    	    };
-    	}
-    	
-    	return SBOFactory;
+
+		if (SBOFactory == null){
+
+			SBOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexSBO )
+			{
+				@Override
+				protected void initialize( Node created, Map<String, Object> properties )
+				{
+					created.setProperty( "id", properties.get( "id" ) );
+					created.addLabel(DynamicLabel.label(indexSBO));
+				}
+			};
+		}
+
+		return SBOFactory;
 	}
 
 
 
 	private static UniqueFactory<Node> getGOFactory() {
-		
-    	if (GOFactory == null){
-    		
-    		GOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexGO )
-    	    {
-    	        @Override
-    	        protected void initialize( Node created, Map<String, Object> properties )
-    	        {
-    	            created.setProperty( "id", properties.get( "id" ) );
-    	           created.addLabel(DynamicLabel.label(indexGO));
-    	        }
-    	    };
-    	}
-    	
-		
+
+		if (GOFactory == null){
+
+			GOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexGO )
+			{
+				@Override
+				protected void initialize( Node created, Map<String, Object> properties )
+				{
+					created.setProperty( "id", properties.get( "id" ) );
+					created.addLabel(DynamicLabel.label(indexGO));
+				}
+			};
+		}
+
+
 		return GOFactory;
 	}
 
 
 	private static UniqueFactory<Node> getKISAOFactory() {
-		
-    	if (KISAOFactory == null){
-    		
-    		KISAOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexKISAO )
-    	    {
-    	        @Override
-    	        protected void initialize( Node created, Map<String, Object> properties )
-    	        {
-    	            created.setProperty( "id", properties.get( "id" ) );
-    	           created.addLabel(DynamicLabel.label(indexKISAO));
-    	        }
-    	    };
-    	}
-    	
-    	return KISAOFactory;
-	}
-	
-	
-	   private static UniqueFactory<Node> getDefaultFactory() {
-					  
-	    	if (UnnamedFactory == null){
-	    		final  String indexUnnamed = Long.toString(System.currentTimeMillis());
-	    		UnnamedFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexUnnamed )
-	    	    {
-	    	        @Override
-	    	        protected void initialize( Node created, Map<String, Object> properties )
-	    	        {
-	    	            created.setProperty( "id", properties.get( "id" ) );
-	    	           created.addLabel(DynamicLabel.label(indexUnnamed));
-	    	        }
-	    	    };
-	    	}
-	    	
-	    	return UnnamedFactory;
+
+		if (KISAOFactory == null){
+
+			KISAOFactory = new UniqueFactory.UniqueNodeFactory( Manager.instance().getDatabase(), indexKISAO )
+			{
+				@Override
+				protected void initialize( Node created, Map<String, Object> properties )
+				{
+					created.setProperty( "id", properties.get( "id" ) );
+					created.addLabel(DynamicLabel.label(indexKISAO));
+				}
+			};
 		}
 
-    
+		return KISAOFactory;
+	}
+
+	private static UniqueFactory<Node> getOrCreateFactoryByName( String ontologyName ) {
+		
+		if( ontologyName == null || ontologyName.isEmpty() )
+			throw new IllegalArgumentException("Ontology is not allowed to be empty or null.");
+		
+		// clean up name
+		ontologyName = ontologyName.replaceAll("[^a-zA-Z0-9_]", "-");
+		
+		// check if factory was previously created
+		if( factoryMap.containsKey(ontologyName) )
+			return factoryMap.get(ontologyName);
+		else {
+			// creates new factory with ontology as name
+			UniqueFactory<Node> factory = new DynamicUniqueNodeFactory( Manager.instance().getDatabase(), ontologyName );
+			factoryMap.put(ontologyName, factory);
+			return factory;
+		}
+		
+	}
+
+	/**
+	 * UniqueNodeFactory, storing the index name.
+	 * To generate dynamic factories for prior unknown ontologies
+	 * 
+	 * @author martin
+	 *
+	 */
+	public static class DynamicUniqueNodeFactory extends UniqueFactory.UniqueNodeFactory {
+		
+		protected String indexName = null;
+		
+		public DynamicUniqueNodeFactory(GraphDatabaseService graphdb, String index) {
+			super(graphdb, index);
+			this.indexName = index;
+		}
+		
+		public DynamicUniqueNodeFactory( Index<Node> index ) {
+            super( index );
+            this.indexName = index.getName();
+        }
+
+		@Override
+		protected void initialize(Node created, Map<String, Object> properties) {
+			created.setProperty( "id", properties.get( "id" ) );
+			created.addLabel( DynamicLabel.label(this.indexName) );
+		}
+		
+	}
 
 
 }
